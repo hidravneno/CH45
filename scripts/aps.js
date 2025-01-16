d5b03efd9342fc60c21b331a2bdda82a210e0379
Hidravneno <aramburoeduardo972@gmail.com>
2025-01-15 20:52:08 -0800

    first commit

---
+++ scripts/aps.js
@@ -0,0 +1,17 @@
+function sayHello() {
+  console.log("Hello");
+}
+
+function init() {
+  console.log("hello world!");
+   sayHi("Adrian");
+  sayHello();
+}
+
+function sayHi(name) {
+  console.log("hi"+name);
+}
+window.onload = init;
+
+
+//variable scope 
